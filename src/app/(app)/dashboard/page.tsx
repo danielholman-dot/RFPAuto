3504963ab102309ac568a4fde808a9aa3171c142
Daniel Holman <danielholman@google.com>
2025-09-25 22:18:04 +0000

    I have a feeling that everything on the dashboard is really big, i want

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -121,8 +121,10 @@ export default function Dashboard() {
             </CardContent>
           </Card>
         </div>
-        <div className="grid gap-4 md:gap-8 lg:grid-cols-1">
-            <Card className="col-span-1">
+        
+        <div className="grid grid-cols-1 items-start gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
+          <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
+            <Card>
                 <CardHeader>
                 <CardTitle>RFP & Project Timelines</CardTitle>
                 <CardDescription>
@@ -133,9 +135,19 @@ export default function Dashboard() {
                     <RfpGanttChart rfps={rfps} />
                 </CardContent>
             </Card>
-        </div>
-        <div className="grid gap-4 md:gap-8 lg:grid-cols-2 xl:grid-cols-3">
-          <Card className="xl:col-span-2">
+             <Card>
+              <CardHeader>
+                <CardTitle>RFP Volume</CardTitle>
+                <CardDescription>
+                  Number of RFPs created over the last 6 months.
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <RfpVolumeChart />
+              </CardContent>
+            </Card>
+          </div>
+          <Card>
             <CardHeader className="flex flex-row items-center">
               <div className="grid gap-2">
                 <CardTitle>Recent RFPs</CardTitle>
@@ -184,17 +196,6 @@ export default function Dashboard() {
                   ))}
                 </TableBody>
               </Table>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardHeader>
-              <CardTitle>RFP Volume</CardTitle>
-              <CardDescription>
-                Number of RFPs created over the last 6 months.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <RfpVolumeChart />
             </CardContent>
           </Card>
         </div>
