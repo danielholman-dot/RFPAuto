995703f4765094887d4d82434b465f7bfa36446c
Rafael Correa (xWF) <correarafael@google.com>
2025-09-26 12:15:29 +0000

    I dont need a dynamic map. We can associate latitude and longitude for e

--- src/app/(app)/metro/page.tsx
+++ src/app/(app)/metro/page.tsx
@@ -14,36 +14,50 @@ import {
   TableRow,
 } from '@/components/ui/table';
 import { metroCodes } from '@/lib/data';
+import { MetroMapChart } from '@/components/metro/metro-map-chart';
 
 export default function MetroPage() {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Metro Codes</CardTitle>
-        <CardDescription>
-          List of active metro codes for projects.
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>Metro Code</TableHead>
-              <TableHead>City</TableHead>
-              <TableHead>State/Province</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            {metroCodes.map((metro) => (
-              <TableRow key={metro.code}>
-                <TableCell className="font-medium">{metro.code}</TableCell>
-                <TableCell>{metro.city}</TableCell>
-                <TableCell>{metro.state}</TableCell>
+    <div className="space-y-6">
+      <Card>
+        <CardHeader>
+          <CardTitle>Metro Codes</CardTitle>
+          <CardDescription>
+            List of active metro codes for projects.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Table>
+            <TableHeader>
+              <TableRow>
+                <TableHead>Metro Code</TableHead>
+                <TableHead>City</TableHead>
+                <TableHead>State/Province</TableHead>
               </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </CardContent>
-    </Card>
+            </TableHeader>
+            <TableBody>
+              {metroCodes.map((metro) => (
+                <TableRow key={metro.code}>
+                  <TableCell className="font-medium">{metro.code}</TableCell>
+                  <TableCell>{metro.city}</TableCell>
+                  <TableCell>{metro.state}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </CardContent>
+      </Card>
+      <Card>
+        <CardHeader>
+          <CardTitle>Metro Locations</CardTitle>
+          <CardDescription>
+            A map showing the distribution of active project metros.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <MetroMapChart metros={metroCodes} />
+        </CardContent>
+      </Card>
+    </div>
   );
 }
