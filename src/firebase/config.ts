c579b7428af943aa7558b74fd6e524bcf1906dfb
Daniel Holman <danielholman@google.com>
2025-09-30 17:14:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "gpo-procurement-sandbox-563772",
+  "appId": "1:1071160844261:web:30144bf94e97dfb6dad90c",
+  "apiKey": "AIzaSyCvHiAeqP8TGLnkcpxRfo7XAwS8AJxpk2k",
+  "authDomain": "gpo-procurement-sandbox-563772.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1071160844261"
+};
