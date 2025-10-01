a4841c33c8cb6b889e4bc2c58f819585c0653cc2
Daniel Holman <danielholman@google.com>
2025-10-01 16:06:51 +0000

    Add security headers and configure Cross-Origin Resource Sharing (CORS)

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,16 @@
+
 import type {NextConfig} from 'next';
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline' *.googleapis.com *.google.com *.gstatic.com;
+  child-src 'self' *.google.com;
+  style-src 'self' 'unsafe-inline' fonts.googleapis.com;
+  img-src * 'self' data:;
+  font-src 'self' fonts.gstatic.com;
+  connect-src *;
+  frame-src 'self' *.google.com;
+`;
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -33,8 +45,43 @@ const nextConfig: NextConfig = {
         hostname: 'storage.googleapis.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'developers.google.com',
+        port: '',
+        pathname: '/**',
       }
     ],
+  },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: ContentSecurityPolicy.replace(/\n/g, ''),
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'origin-when-cross-origin',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          }
+        ],
+      },
+    ]
   },
 };
 
