6637c1c349defd9710126991261cb81d8f44bb84
Rafael Correa (xWF) <correarafael@google.com>
2025-10-07 17:35:39 +0000

    Fix this. Firebase Storage: No default bucket found. Did you set the 'st

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,7 @@
 export const firebaseConfig = {
   "projectId": "gpo-procurement-sandbox-563772",
   "appId": "1:1071160844261:web:30144bf94e97dfb6dad90c",
+  "storageBucket": "gpo-procurement-sandbox-563772.appspot.com",
   "apiKey": "AIzaSyCvHiAeqP8TGLnkcpxRfo7XAwS8AJxpk2k",
   "authDomain": "gpo-procurement-sandbox-563772.firebaseapp.com",
   "measurementId": "",
