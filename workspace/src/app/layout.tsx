360a0dbc0074d97ee16e169d3022005da1c8af25
Daniel Holman <danielholman@google.com>
2025-10-16 18:23:47 +0000

    Okay, we have a new error in this build! This time it's a "Module not fo

--- workspace/src/app/layout.tsx
+++ workspace/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css';
+import '@/app/globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import { cn } from '@/lib/utils';
 import { FirebaseClientProvider } from '@/firebase';
