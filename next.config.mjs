4fcfb8d3657c97118714fcd022398d329c812c8a
Daniel Holman <danielholman@google.com>
2025-10-28 21:12:21 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,13 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    experimental: {
+        // This is required to allow the Next.js dev server to accept requests from the
+        // Firebase Studio environment.
+        allowedDevOrigins: [
+            "https://6000-firebase-rfp-automation-1761668564232.cluster-thle3dudhffpwss7zs5hxaeu2o.cloudworkstations.dev",
+        ],
+    },
 };
 
 export default nextConfig;
