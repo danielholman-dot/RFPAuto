d5f424cf3a24223e1c650a43ce8fcbd2e18b0139
Daniel Holman <danielholman@google.com>
2025-10-28 17:51:40 +0000

    There was an unknown error while provisioning your cloud resources.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -2,7 +2,7 @@ export const firebaseConfig = {
   apiKey: "AIzaSyBs5bzkVakKscPXnkvuRO-67Eim0tXO8xE",
   authDomain: "constructionrfp-sandbox-943476.firebaseapp.com",
   projectId: "constructionrfp-sandbox-943476",
-  storageBucket: "constructionrfp-sandbox-943476.appspot.com",
+  storageBucket: "constructionrfp-sandbox-943476.firebasestorage.app",
   messagingSenderId: "24350062917",
   appId: "1:24350062917:web:e62421339d34c35fb8262c",
   measurementId: "G-WNLYG1EWXL"
