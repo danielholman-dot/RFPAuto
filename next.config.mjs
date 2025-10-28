1b2f70b400a6fb176dbce6f4a579be4894157845
Daniel Holman <danielholman@google.com>
2025-10-28 21:08:35 +0000

    The next.config.mjs file should be located at the root of your project d

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    allowedDevOrigins: [
-        "https://6000-firebase-studio-1758829412721.cluster-thle3dudhffpwss7zs5hxaeu2o.cloudworkstations.dev",
-    ]
-  },
+  reactStrictMode: true,
 };
 
 export default nextConfig;
