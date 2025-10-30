a08f85155bbef10d4c7dc1be659e16ee984fe025
Rafael Correa (xWF) <correarafael@google.com>
2025-10-30 17:44:16 +0000

    We still are not able to advance to the dashboard. The login i think sti

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,6 +1,8 @@
+
 export const firebaseConfig = {
   "projectId": "constructionrfp-sandbox-943476",
   "appId": "1:24350062917:web:65562a7260a7e7c9b8262c",
+  "storageBucket": "constructionrfp-sandbox-943476.appspot.com",
   "apiKey": "AIzaSyBs5bzkVakKscPXnkvuRO-67Eim0tXO8xE",
   "authDomain": "constructionrfp-sandbox-943476.firebaseapp.com",
   "measurementId": "",
