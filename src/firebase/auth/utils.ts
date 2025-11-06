bad300bb9df1c07dae1d96202374a2b61d51d75e
Rafael Correa (xWF) <correarafael@google.com>
2025-11-06 15:28:48 +0000

    Try fixing this error: `Console FirebaseError: Firebase: Error (auth/int

--- src/firebase/auth/utils.ts
+++ src/firebase/auth/utils.ts
@@ -3,9 +3,14 @@
 
 import { getAuth, signInWithRedirect, GoogleAuthProvider, Auth } from "firebase/auth";
 import { toast } from "@/hooks/use-toast";
+import { firebaseConfig } from "../config";
 
 export const signInWithGoogle = (auth: Auth) => {
   const provider = new GoogleAuthProvider();
+  // Explicitly setting the client ID can resolve some auth/internal-error issues.
+  provider.setCustomParameters({
+    'webClientId': firebaseConfig.webClientId,
+  });
   // We use signInWithRedirect which is more robust than signInWithPopup.
   // The result is handled in the FirebaseProvider.
   signInWithRedirect(auth, provider).catch((error: any) => {
@@ -17,5 +22,3 @@ export const signInWithGoogle = (auth: Auth) => {
     });
   });
 };
-
-    
