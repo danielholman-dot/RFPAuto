894b29f4f14b525bf923863bc050b4a66e4749f8
Daniel Holman <danielholman@google.com>
2025-11-11 21:46:55 +0000

    Add the Google G as the favicon.ico image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export default function RootLayout({
       <head>
         <title>MARCUS Automation Suite</title>
         <meta name="description" content="MARCUS Automation Suite for RFP Management" />
+        <link rel="icon" href="https://www.google.com/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
